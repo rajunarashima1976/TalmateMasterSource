151616305358a4e8681b5d35deaa6ca16fe0facc
User6 <User6@CBAPOOL-0>
2020-07-14 07:49:14 +0000

    Add All changes

---
+++ TalmateSourceCode/Controllers/DemandController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TalmateSourceCode.DemandService;
+using TalmateSourceCode.EntityModel;
+
+namespace TalmateSourceCode.Controllers
+{
+    [Route("api/demand")]
+    [ApiController]
+    public class DemandController : ControllerBase
+    {
+        private readonly IDemandService _demandService;
+        private readonly IMapper _mapper;
+        public DemandController(IDemandService demandService, IMapper mapper)
+        {
+            _demandService = demandService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var response = await _demandService.Get();
+            return Ok(response);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Demand demand)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var response = await _demandService.Post(demand);
+            return Ok(response);
+        }
+
+
+    }
+}
---
+++ TalmateSourceCode/Controllers/UsersController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TalmateSourceCode.EntityModel;
+using TalmateSourceCode.UserService;
+
+namespace TalmateSourceCode.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+        public UsersController(IUserService userService, IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Authenticate([FromBody] User model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var result = await _userService.Authenticate(model.Username, model.Password);
+
+            if (result == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(result);
+        }
+        [HttpGet]
+
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _userService.GetAll();
+            return Ok(users);
+        }
+        [HttpGet]
+
+        [Route("id")]
+        public async Task<IActionResult> Get(int Id)
+        {
+            var users = await _userService.GetById(Id);
+            return Ok(users);
+        }
+
+    }
+}
